cfe9b8d404ecea5a69062ee7ce86930223acc885
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-21 18:40:52 -0700

    Refactor Bindings and Singletons to use Record type

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,7 @@
 export type Factory<T> = () => T;
 
-export type Bindings = {
-  [key: string]: Factory<unknown> | unknown;
-};
-export type Singletons = {
-  [key: string]: Factory<unknown> | unknown;
-};
+export type Bindings = Record<string, Factory<unknown> | unknown>;
+export type Singletons = Record<string, Factory<unknown> | unknown>;
 
 export interface Dependency {
   key: string;
